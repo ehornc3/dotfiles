aa00bd16af79dea8932afb2820f123ede355d611
Ethan <ethan@etcho.net>
2024-04-01 12:28:38 -0400

    fix stuff

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo tar -C /opt -xzf nvim-linux64.tar.gz
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # oh-my-zsh plugins
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ./.oh-my-zsh/custom/plugins/zsh-autocomplete
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ./.omz-custom/plugins/zsh-autocomplete
 
 # nv-chad
 git clone https://github.com/NvChad/starter ~/.config/nvim
