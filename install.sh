c5a01e5b5b41a2e8cfe73102a3f6c7e59121375f
Ethan <ethan@etcho.net>
2024-04-01 14:41:04 -0400

    nvim link

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
+echo "export PATH=\"$PATH:/opt/nvim-linux64/src/\"" >> .zshrc
 
 # get oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
