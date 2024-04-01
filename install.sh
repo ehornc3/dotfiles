ff3623aa5b6f130b3b52f5e8a731ab2d65cc68a7
Ethan <ethan@etcho.net>
2024-04-01 15:05:46 -0400

    should be better

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
+# unlink before installing deps
+stow -D .
 # get neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
 echo "export PATH=\"$PATH:/opt/nvim-linux64/bin/\"" >> .zshrc
 
 # get oh-my-zsh
-rm -r ~/.oh-my-zsh
+rm -rf ~/.oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
+rm -rf ~/.zshrc
 
 # oh-my-zsh plugins
 
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.omz-custom/plugins/zsh-autocomplete
 
 # nv-chad
-rm -r ~/.config/nvim
+rm -rf ~/.config/nvim
 git clone https://github.com/NvChad/starter ~/.config/nvim
-rm -r ~/.config/nvim/lua/custom
 
 # link dotfiles
 stow .
